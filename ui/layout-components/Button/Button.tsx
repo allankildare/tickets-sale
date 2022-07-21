a7742d63c11d4cb2c473e20f6395f363705608aa
allankildare <allankildaredev@gmail.com>
2022-07-21 17:10:35 -0300

    chore: adding icon to button

--- ui/layout-components/Button/Button.tsx
+++ ui/layout-components/Button/Button.tsx
@@ -1,5 +1,13 @@
 import { StyledButton } from './styles'
 import type { ButtonProps } from './interface'
+import { Icon } from 'ui/layout-components'
+import { grayScale, color as themeColor } from 'ui/theme'
+
+function iconColor({ isDisabled, color }: { isDisabled?: boolean, color: 'primary' | 'secondary' }) {
+    if(isDisabled) return grayScale.gray30
+    if(color === 'primary') return '#fff'
+    if(color === 'secondary') return themeColor.message
+}
 
 export function Button(props: ButtonProps) {
     const { color = 'primary', size = 'medium', disabled, children, icon } = props
@@ -11,6 +19,13 @@ export function Button(props: ButtonProps) {
             size={size}
         >
             { children }
+            {icon && (
+                <Icon
+                    name={icon}
+                    size="12px"
+                    stroke={iconColor({ isDisabled: disabled, color: color })}
+                />
+            )}
         </StyledButton>
     )
 }
--- ui/layout-components/Button/interface.ts
+++ ui/layout-components/Button/interface.ts
@@ -1,7 +1,9 @@
+import type { Names } from 'layout-components/Icon'
+
 export interface ButtonProps {
     size?: 'large' | 'medium' | 'small'
     color?: 'primary' | 'secondary'
     disabled?: boolean
     children: string
-    icon?: string
+    icon?: Names
 }
