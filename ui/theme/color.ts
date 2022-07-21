c3d9ad65eb2fab95889f11898a965a1c9e93b99c
allankildare <allankildaredev@gmail.com>
2022-07-21 00:20:07 -0300

    feat: creating gray scale and colors for theme

---
+++ ui/theme/color.ts
@@ -0,0 +1,7 @@
+export const color = {
+    message: 'rgb(64, 112, 244)',
+    success: 'rgb(11, 176, 123)',
+    warning: 'rgb(255, 173, 13)',
+    error: 'rgb(240, 61, 61)',
+    info: 'rgb(60, 76, 112)'
+}
