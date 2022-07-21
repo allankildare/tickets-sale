91f62172f3f57b51b77b5fca12b7838a6e68b472
allankildare <allankildaredev@gmail.com>
2022-07-21 15:22:55 -0300

    refactor: icons now are just d atrributes of svg path

--- ui/icons/index.ts
+++ ui/icons/index.ts
@@ -1,6 +1,10 @@
-export { ArrowLeft } from './arrow-left'
-export { ArrowRight } from './arrow-right'
-export { Check } from './check'
-export { MenuIcon } from './menu-icon'
-export { UserIcon } from './user-icon'
-export { ZapIcon } from './zap-icon'
+const icons =  {
+    'arrow-left': 'M19 12.35H5M12 19.35l-7-7 7-7',
+    'arrow-right': 'M5 12.35h14M12 5.35l7 7-7 7',
+    check: 'M20 6.35l-11 11-5-5',
+    menu: 'M3 12.35h18M3 6.35h18M3 18.35h18',
+    user: 'M20 21.35v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2M12 11.35a4 4 0 100-8 4 4 0 000 8z',
+    zap: 'M13 2.35l-10 12h9l-1 8 10-12h-9l1-8z'
+}
+
+export default icons
