7a497661887f30a652a1437ef547c06f219dcfc1
allankildare <allankildaredev@gmail.com>
2022-07-21 15:21:40 -0300

    feat: theme provider wrapper in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import 'ress'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThemeProvider } from 'styled-components'
+import * as theme from 'ui/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
