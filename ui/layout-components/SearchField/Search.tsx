47df42b785328512f6b4421347683d72cc9d11bf
Allan Kildare <allankildaredev@gmail.com>
2022-07-23 19:03:57 -0300

    feat: adding SearchField layout component

---
+++ ui/layout-components/SearchField/Search.tsx
@@ -0,0 +1,38 @@
+import { useState, MouseEvent } from 'react'
+import { Icon } from '..'
+import { StyledSearch, SearchDiv } from './styles'
+
+interface SearchProps {
+  placeholder?: string
+  onClick?: () => void
+}
+
+export function Search(props: SearchProps) {
+  const { placeholder, onClick } = props
+  const [isOnFocus, setIsOnFocus] = useState(false)
+
+  const handleFocus = () => {
+    setIsOnFocus(isOnFocus => !isOnFocus)
+  }
+
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    event.preventDefault()
+    if(onClick) onClick()
+  }
+
+  return (
+    <>
+      <SearchDiv>
+        <StyledSearch
+          type="text"
+          placeholder={placeholder}
+          onFocus={handleFocus}
+          onBlur={handleFocus}
+        />
+        <div className={`icon ${isOnFocus && 'focused'}`} onClick={handleClick}>
+          <Icon name="search" />
+        </div>
+      </SearchDiv>
+    </>
+  )
+}
