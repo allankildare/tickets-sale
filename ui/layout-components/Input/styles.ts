439093688269e37370a11c00204a36bf04f15c9a
Allan Kildare <allankildaredev@gmail.com>
2022-07-23 15:54:22 -0300

    style: disabled text input stylization

--- ui/layout-components/Input/styles.ts
+++ ui/layout-components/Input/styles.ts
@@ -10,8 +10,11 @@ export const InputField = styled.input<InputProps>`
     width: 100%;
     border: 1px solid ${({ theme }) => theme.grayScale.gray10};
 
-    border-right: ${({ success, error }) => (success || error) ? 'none' : ''};
-    border-radius: ${({ theme, success, error }) => (success || error) ? `${theme.radius.default} 0 0 ${theme.radius.default}` : theme.radius.default};
+    border-right: ${({ success, error, disabled }) => ((success || error) && !disabled) ? 'none' : ''};
+    border-radius: ${({ theme, success, error, disabled }) => {
+        if((success || error) && !disabled) return `${theme.radius.default} 0 0 ${theme.radius.default}`
+        return theme.radius.default
+    }};
     color: ${({ theme }) => theme.brandColor.black};
     border-color: ${({ theme, success, error }) => {
         if(success) return theme.color.success
@@ -26,13 +29,21 @@ export const InputField = styled.input<InputProps>`
             return theme.color.black
         }};
     }
+
+    &:disabled {
+        background-color: #EDF0F5;
+        border-color: ${({ theme }) => theme.grayScale.gray10};
+    }
 `
 export const TextField = styled.div<InputProps>`
     display: flex;
     max-width: 400px;
 
     & > div.icon {
-        background-color: #FAFAFC;
+        background-color: '#FAFAFC';
+        display: ${({ disabled }) => {
+            if(disabled) return 'none'
+        }};
         border: 1px solid ${({ theme }) => theme.grayScale.gray10};
         border-radius: ${({ theme }) => `0 ${theme.radius.default} ${theme.radius.default} 0`};
         border-left: none;
@@ -42,8 +53,10 @@ export const TextField = styled.div<InputProps>`
             if(success) return theme.color.success
             if(error) return theme.color.error
             return ''
-    }};
+        }};
     }
+
+    
 
     svg {
         display: inline-block;
