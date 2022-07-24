7aede17d3a3004f8d0331a46fae5e3d96be3db1a
Allan Kildare <allankildaredev@gmail.com>
2022-07-24 19:25:24 -0300

    chore: adding focus stylization, event and data types

--- ui/layout-components/DateField/DateField.tsx
+++ ui/layout-components/DateField/DateField.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import Calendar from 'react-calendar'
 import { Icon } from 'layout-components/Icon'
 import { CalendarDiv, StyledDate } from './styles'
@@ -7,33 +7,47 @@ import dayjs from 'dayjs'
 
 export function DateField(props: any) {
   const [isCalendarOpen, setIsCalendarOpen] = useState(false)
-  const [value, onChange] = useState(new Date())
+  const [value, onChange] = useState('')
   const [calendarValue, setCalendarValue] = useState(value)
+  const [isOnFocus, setIsOnFocus] = useState(false)
 
   const openCalendar = () => {
     setIsCalendarOpen(isCalendarOpen => !isCalendarOpen)
   }
 
-  const handleCalendarChange = ({ target }) => {
+  const handleCalendarChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     setCalendarValue(target.value)
+  }
+
+  const handleFocus = () => {
+    setIsOnFocus(isOnFocus => !isOnFocus)
   }
 
   return (
     <>
       <CalendarDiv>
-        <StyledDate type="text" onChange={handleCalendarChange} value={calendarValue} />
-        <div className="icon" onClick={openCalendar}>
+        <StyledDate
+          type="text"
+          onChange={handleCalendarChange}
+          onFocus={handleFocus}
+          onBlur={handleFocus}
+          value={calendarValue}
+        />
+        <div
+          className={`icon ${isOnFocus && 'focused'}`}
+          onClick={openCalendar}
+        >
           <Icon name="calendar" />
         </div>
       </CalendarDiv>
       <If condition={isCalendarOpen}>
         <Calendar
-            onChange={onChange}
-            onClickDay={(value) => {
-                const formattedDate = dayjs(value).format('DD/MM/YYYY') as string
-                setCalendarValue(formattedDate)
-            }}
-            value={value}
+          onChange={onChange}
+          onClickDay={(value: string) => {
+            const formattedDate = dayjs(value).format('DD/MM/YYYY') as string
+            setCalendarValue(formattedDate)
+          }}
+          value={value}
         />
       </If>
     </>
