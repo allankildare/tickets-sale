da9c5595a6b4933f671a422e97212925db66a814
Allan Kildare <allankildaredev@gmail.com>
2022-07-25 01:14:27 -0300

    feat: adding Header to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Header } from 'ui/components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className="container">
-      Começando projeto...
+      <Header />
     </div>
   )
 }
