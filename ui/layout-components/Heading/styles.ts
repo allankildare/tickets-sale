e78afb2003df0feb3e1dd3cc0bf583d1991906a5
Allan Kildare <allankildaredev@gmail.com>
2022-07-25 16:35:17 -0300

    style: theme font color in Heading

--- ui/layout-components/Heading/styles.ts
+++ ui/layout-components/Heading/styles.ts
@@ -8,4 +8,5 @@ export const StyledHeading = styled.h1<StyledProps>`
     font-size: ${({ type }) => (type === '1') ? '40px' : (type === '2') ? '24px' : '22px'};
     letter-spacing: ${({ type }) => (type === '1') ? '-2' : (type === '2') ? '-1' : '0'};
     line-height: ${({ type }) => (type === '1') ? '45px' : (type === '2') ? '30px' : '28px'};
+    color: ${({ theme }) => theme.brandColor.black}
 `
