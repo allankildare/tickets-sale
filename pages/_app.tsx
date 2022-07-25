dc26a3577af3dcf99358d9b0131c060f3f2061e5
Allan Kildare <allankildaredev@gmail.com>
2022-07-25 01:07:17 -0300

    chore: query client provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,16 @@ import 'react-calendar/dist/Calendar.css';
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import * as theme from 'ui/theme'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from 'clients'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   )
 }
 
