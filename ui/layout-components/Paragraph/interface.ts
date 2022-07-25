b601689dfd7f8bef81b2b3d68612049cda53728e
Allan Kildare <allankildaredev@gmail.com>
2022-07-25 16:35:43 -0300

    chore: adjusting types of color prop

--- ui/layout-components/Paragraph/interface.ts
+++ ui/layout-components/Paragraph/interface.ts
@@ -1,8 +1,10 @@
+import { BrandColor, Color } from 'types'
+
 export interface ParagraphProps {
     size?: 'large' | 'medium' | 'normal' | 'small'
     as?: 'p' | 'span' | 'label'
     children: string
-    color?: string
+    color?: Color | BrandColor
     isError?: boolean
     isBold?: boolean
 }
